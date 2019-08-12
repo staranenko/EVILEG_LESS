31450d1ea501fba0679f756b95c883f073741e74
s.v.taranenko <sergey.taranenko@gmail.com>
2019-08-12 17:31:13 +0700

    Midify project 3

--- 003_Settings/main.cpp
+++ 003_Settings/main.cpp
@@ -4,6 +4,17 @@
 
 int main(int argc, char *argv[])
 {
+    /* Для адекватной регистрации приложения в операционной системе
+     * необходимо установить название организации, домен организации,
+     * а также название приложения.
+     * Все настройки будут храниться в операционной системе под этими
+     * учетными данными приложения
+     * */
+
+    QCoreApplication::setOrganizationName(ORGANIZATION_NAME);
+    QCoreApplication::setOrganizationDomain(ORGANIZATION_DOMEN);
+    QCoreApplication::setApplicationName(APPLIKATION_NAME);
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
--- 003_Settings/mainwindow.h
+++ 003_Settings/mainwindow.h
@@ -2,6 +2,18 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QCloseEvent>
+#include <QSystemTrayIcon>
+#include <QAction>
+#include <QSettings>
+#include <QMessageBox>
+
+/* Defining */
+#define ORGANIZATION_NAME "EVELEG"
+#define ORGANIZATION_DOMAIN "www.eweleg.com"
+#define APPLICATION_NAME "QSettings Program"
+
+#define SETTINGS_TRAY "settings/tray"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +27,18 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+protected:
+    void closeEvent(QCloseEvent * event);
+
+private slots:
+    void iconActivated(QSystemTrayIcon::ActivationReason reason);
+
+    /* Слот для кнопки, запускающей сохранение настроек приложения
+     * */
+    void on_saveButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QSystemTrayIcon *trayIcon;
 };
 #endif // MAINWINDOW_H
